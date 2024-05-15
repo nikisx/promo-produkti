0a33129a9671591a18a636c9d6dd873cd70f8cfc
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2024-05-15 22:11:05 +0300

    styles for the programming book

--- src/components/Products/ProgrammingBook.vue
+++ src/components/Products/ProgrammingBook.vue
@@ -9,71 +9,53 @@
                           </ul>
                        </div>
                        <p style="font-size: 20px;" class="mobile-font-size">При поръчки над 1 брой получавате БЕЗПЛАТНА доставка и 20% намаление</p>
-                       <div >
+                       <!-- <div >
                           <ul class="social_icon">
                              <li> <a href="https://www.facebook.com/prodavalniktopceni" target="_blanc"><i class="fa fa-facebook" aria-hidden="true"></i>
                                 </a>
                              </li>
-                             <!-- <li> <a href="#"><i class="fa fa-twitter"></i></a></li>
-                             <li> <a href="#"> <i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                             <li> <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i>
-                                </a>
-                             </li> -->
+                            
                           </ul>
-                       </div>
+                       </div> -->
                       
                     </div>
                  </div>
               </div>
-              <section class="flex first-section">
+              <section style="    background: rgb(251, 244, 233) !important;" class="flex first-section">
                   <div class="wd-60">
-                      <h1 style="color: rgb(8, 83, 149);width: 70%;margin: 0 auto;">Играчката, която най-накрая успява да забавлява децата и да ви осигури свободно време!</h1>
-                      <img src="../../assets/images/Elegant_-_GIF_480x480.webp" alt="">
+                      <h1 style="color: rgb(8, 83, 149);width: 90%;margin: 0 auto;">
+                        Въведете децата си в света на <b>програмирането</b> по един <b>лесен</b> и <b>увлекателен</b> начин
+                       </h1>
+                      <img src="../../assets/images/book-over-head.jpg" style="border-radius: 25px;" alt="">
                       <ul class="pens-description">
-                          <li> <i style="font-size: 30px;margin-right: 6px;color: green;" class="fa fa-check-circle-o" aria-hidden="true"></i>Рисунките плават във вода!</li>
-                          <li><i style="font-size: 30px;margin-right: 6px;color: green;" class="fa fa-check-circle-o" aria-hidden="true"></i>Лесно за ползване и наслада!</li>
-                          <li><i style="font-size: 30px;margin-right: 6px;color: green;" class="fa fa-check-circle-o" aria-hidden="true"></i>Превръща всяка рисунка в татуировка!</li>
-                          <li><i style="font-size: 30px;margin-right: 6px;color: green;" class="fa fa-check-circle-o" aria-hidden="true"></i>Ангажираща дейност!</li>
-                          <li><i style="font-size: 30px;margin-right: 6px;color: green;" class="fa fa-check-circle-o" aria-hidden="true"></i>Безопасен за използване от деца!</li>
-                          <li><i style="font-size: 30px;margin-right: 6px;color: green;" class="fa fa-check-circle-o" aria-hidden="true"></i>Играчка, която забавлява децата!</li>
-                          <li><i style="font-size: 30px;margin-right: 6px;color: green;" class="fa fa-check-circle-o" aria-hidden="true"></i>Дава Ви свободно време като родители!</li>
+                          <li style="display: flex;gap: 5px;margin-bottom: 10px;"> <i style="font-size: 30px;margin-right: 6px;color: green;margin-left: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i><p style="font-size:18px;">Подготовка за бъдещето: Инвестиция в знания за добре платена бъдеща кариера.</p></li>
+                          <li style="display: flex;gap: 5px;margin-bottom: 10px;"><i style="font-size: 30px;margin-right: 6px;color: green;margin-left: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i><p style="font-size:18px;">Лесно обучение: Ясни инструкции, подходящи за деца.</p></li>
+                          <li style="display: flex;gap: 5px;margin-bottom: 10px;"><i style="font-size: 30px;margin-right: 6px;color: green;margin-left: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i><p style="font-size:18px;">Практични упражнения: Упражнения и проекти за практика.</p></li>
+                          <li style="display: flex;gap: 5px;margin-bottom: 10px;"><i style="font-size: 30px;margin-right: 6px;color: green;margin-left: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i><p style="font-size:18px;">Основни технологии: Усвояване на HTML, CSS и JavaScript.</p></li>
+                          <li style="display: flex;gap: 5px;margin-bottom: 10px;"><i style="font-size: 30px;margin-right: 6px;color: green;margin-left: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i><p style="font-size:18px;">Логическо мислене: Развитие на критично мислене.</p></li>
+                          <li style="display: flex;gap: 5px;margin-bottom: 10px;"><i style="font-size: 30px;margin-right: 6px;color: green;margin-left: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i><p style="font-size:18px;">Креативност: Възможност за създаване на уникални уеб страници.</p></li>
+                          <li style="display: flex;gap: 5px;margin-bottom: 10px;"><i style="font-size: 30px;margin-right: 6px;color: green;margin-left: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i><p style="font-size:18px;">Дава Ви свободно време като родители!</p></li>
                       </ul>
                   </div>
                   <div class="wd-50">
-                      <h1 style="font-style: italic;color: rgb(8, 83, 149);">"Къде е било това нещо цял живот?"</h1>
-                      <img src="../../assets/images/Magic_Painting_Pens_By_MommysMagic.co_website_official_retailer_of_Floating_Drawing_Pens_600x600.webp" alt=""/>
-                      <p style="font-size: 20px">Оставете децата да се насладят на различни цветове и възможности за невероятни рисунки! 
-                      Децата ви ще прекарат много време в игра с нашите флумастери, докато вие вършите работа. 
-                      Няма да чувате повече думите,
-                      "Мамо, татко, скучно ми е!"</p>
-                      <button @click="toggleSidebar()" class="gf_button"> ИСКАМ ДА ПОРЪЧАМ!</button>
+                      <h1 style="font-style: italic;color: rgb(8, 83, 149);">Програмиране за деца Забавление и обучение в едно!</h1>
+                      <img style="    border-radius: 170px;" src="../../assets/images/happy-little-girl-holding-a-book-mockup-at-the-library-a19273.jpg" alt=""/>
+                      <p style="font-size: 20px; margin-top: 2rem;"> Представете си света, където вашето дете не само използва технологиите, но и ги създава! <br> Нашата книга
+                         <b>"Младият програмист"</b> е ключът към това вълнуващо пътешествие.</p>
+                      <button @click="toggleSidebar()" class="gf_button"> ПОРЪЧАЙ СЕГА!</button>
                   </div>
               </section>
               <section style="background: #3c91e0d6!important; padding: 35px 0">
-                  <h1 style="font-weight: bold; color: white">КАК РАБОТИ?</h1>
+                  <h1 style="font-weight: bold; color: white">Стъпка по стъпка към програмирането</h1>
                   <hr style="height: 5px;background: white;width: 85%;">
-                  <p style="color: white;font-size: 20px;font-weight: bold;width: 50%;margin: 0 auto;">Всичко става бързо и лесно в 3 стъпки!
-                  (не е необходима специална вода!)
-                  Готови сте за Игра!</p>
-                  <div style="display: flex;align-items: baseline;margin-top: 30px;gap:10px">
-                      <div style="width: 33%">
-                          <h2 style="font-weight: bold;color: white;">Стъпка 1</h2>
-                          <img src="../../assets/images/HowToUse1.png" alt="">
-                          <p style="font-weight: bold;color: white;">Нарисувайте на гърба на вълшебната лъжица
-                          * Уверете се, че лъжицата е суха, преди да започнете да рисувате.</p>
-                      </div>
-                      <div style="width: 33%">
-                          <h2 style="font-weight: bold;color: white;">Стъпка 2</h2>
-                          <img src="../../assets/images/HowToUse2.png" alt="">
-                          <p style="font-weight: bold;color: white;">Изчакайте рисунката върху лъжицата да изсъхне, след това прехвърлете БАВНО в дълбока чиния пълна с вода и се наслаждавайте!</p>
-                      </div>
-                      <div style="width: 33%">
-                          <h2 style="font-weight: bold;color: white;">Стъпка 3</h2>
-                          <img src="../../assets/images/HowToUse3.png" alt="">
-                          <p style="font-weight: bold;color: white;">Гледайте как децата ви си играят часове наред!</p>
-                      </div>
-                  </div>
-                  <button @click="toggleSidebar()" class="gf_button"> ИСКАМ ДА ПОРЪЧАМ!</button>
+                  <p style="color: white;font-size: 20px;font-weight: bold;width: 90%;margin: 0 auto;    margin-bottom: 15px;">
+                    Представете си гордостта на детето си, когато види собствения си сайт онлайн! Всяка нова страница е нова възможност за успех и развитие.
+                </p>
+                <img src="../../assets/images/boy.jpg" style="    border-radius: 25px;" alt="">
+                <p style="color: white;font-size: 20px;font-weight: bold;width: 90%;margin: 0 auto;margin-top: 15px;">
+                    Започнете още днес и наблюдавайте как вашето дете расте като малък програмист. Поръчайте книгата и се включете в нашите уроци.
+                </p>
+                  <button @click="toggleSidebar()" class="gf_button"> ПОРЪЧАЙ СЕГА!</button>
               </section>
               <section class="flex" style="background: #fbf4e9!important; justify-content: space-evenly;padding: 35px 0">
                   <div>
@@ -82,15 +64,15 @@
                   </div>
                   <div>
                       <ul style="width: 100%;" class="pens-description">
-                          <li> <i style="font-size: 30px;margin-right: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i>Флумастерите са нови и иновативни!</li>
-                          <li> <i style="font-size: 30px;margin-right: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i>В комплетка влизат 11 боря флумастери и лъжичка!</li>
-                          <li><i style="font-size: 30px;margin-right: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i>Рисунките плават!</li>
-                          <li><i style="font-size: 30px;margin-right: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i>Подходящо за деца от 2 години нагоре!</li>
-                          <li><i style="font-size: 30px;margin-right: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i>Може да ги превърнтете в татуировка!</li>
-                          <li><i style="font-size: 30px;margin-right: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i>Бърза доставка (1-2 дни)!</li>
+                        <li><i style="font-size: 30px;margin-right: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i>Подготовка за бъдещето: Инвестиция в знания за добре платена бъдеща кариера.</li>
+                          <li> <i style="font-size: 30px;margin-right: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i>Лесно обучение: Ясни инструкции, подходящи за деца.</li>
+                          <li> <i style="font-size: 30px;margin-right: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i>Практични упражнения: Упражнения и проекти за практика.</li>
+                          <li><i style="font-size: 30px;margin-right: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i>Основни технологии: Усвояване на HTML, CSS и JavaScript.</li>
+                          <li><i style="font-size: 30px;margin-right: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i>Логическо мислене: Развитие на критично мислене.</li>
+                          <li><i style="font-size: 30px;margin-right: 6px;" class="fa fa-check-circle-o" aria-hidden="true"></i>Креативност: Възможност за създаване на уникални уеб страници.</li>
                       </ul> 
                       <p class="risk-free-text">Пробвайте за <span style="color: rgb(232, 78, 7)">60 дни</span> без риск!Гаранция за връщане на парите</p>
-                      <button @click="toggleSidebar()" class="gf_button"> ИСКАМ ДА ПОРЪЧАМ!</button>
+                      <button @click="toggleSidebar()" class="gf_button"> ПОРЪЧАЙ СЕГА!</button>
                   </div>
               </section>
               <section style="background: #b7e449;margin-top: 40px;padding: 30px;">
@@ -113,7 +95,7 @@
                   <p style="font-size: 20px;padding: 20px;">Препоръчваме ви да използвате наистина дълбока и широка купа (а не чиния) у дома или просто да се насладите на нея във ваната заедно с малчуганите! Просто се уверете, че лъжичките са сухи, преди да рисувате върху тях.</p>
                   </div>
               </div>
-              <button @click="toggleSidebar()" style="display: block;margin: 0 auto;margin-top: 50px;" class="gf_button"> ИСКАМ ДА ПОРЪЧАМ!</button>
+              <button @click="toggleSidebar()" style="display: block;margin: 0 auto;margin-top: 50px;" class="gf_button"> ПОРЪЧАЙ СЕГА!</button>
           </section>
           <section style="padding: 20px">
               <p style="font-size: 35px; font-weight: bold;line-height: 32px;">Поради ограничени количества цената на нашите флумастери е намалена с 50%</p>
